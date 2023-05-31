93ff17d9cf831a01f30bc8802f7f9e31f03791b0
germauricio <mauriciogpauluk@gmail.com>
2023-05-31 00:18:20 -0300

    cors

--- index.ts
+++ index.ts
@@ -4,27 +4,30 @@ const cors = require('cors');
 const app = express();
 
 const corsOrigin ={
-  origin:'http://localhost:3000', //or whatever port your frontend is using
-  credentials:true,            
+  origin:'*', //or whatever port your frontend is using
   optionSuccessStatus:200
 }
 app.use(cors(corsOrigin));
 
-app.use(function(req: any, res: any, next: any) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-  next();
-});
 
 const httpServer = app.listen('8080');
 
+
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: '*',
-    methods: ["GET", "POST"]
+    origin: "*",
+    methods: ["PUT", "GET", "POST", "DELETE", "OPTIONS"],
+    credentials: false
   },
+});
+
+
+io.engine.on("initial_headers", (headers: any, req: any) => {
+  headers["Access-Control-Allow-Origin"] = "*";
+});
+
+io.engine.on("headers", (headers: any, req: any) => {
+  headers["Access-Control-Allow-Origin"] = "*"; // url to all
 });
 
 const messages: any = [];
