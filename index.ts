6863c7097919d2ae63d9321cb4346c9bf7fbb283
germauricio <mauriciogpauluk@gmail.com>
2023-05-31 00:08:24 -0300

    cors

--- index.ts
+++ index.ts
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+
+app.use(function(req: any, res: any, next: any) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  next();
+});
+
 const httpServer = app.listen('8080');
 
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: '*'
   }
 });
 
